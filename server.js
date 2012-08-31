db7882088a62ee0d9a0ddcd7aed6d22d16f7ecdb
Mike Pennisi <mike@mikepennisi.com>
2012-08-31 16:32:54 -0400

    Implement HTTPS and secure sessions accessible by socket.io

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
 var express = require("express");
+var RedisStore = require("connect-redis")(express);
 var passport = require("passport");
 var _ = require("underscore");
 
-//var OAuthStrategy = require("passport-oauth").OAuthStrategy;
 var TwitterStrategy = require("passport-twitter").Strategy;
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
-var TwitterCreds = require("./credentials/twitter.json");
-var GoogleCreds = require("./credentials/google.json");
+// Credentials, stored in non-version-controlled files
+var CREDS = {
+	oauth: {
+		twitter: require("./credentials/oauth/twitter.json"),
+		google: require("./credentials/oauth/google.json")
+	},
+	ssl: {
+		key: fs.readFileSync("./credentials/ssl/privatekey.pem").toString(),
+		cert: fs.readFileSync("./credentials/ssl/certificate.pem").toString()
+	}
+};
 
 passport.serializeUser(function(user, done) {
 	done(null, user.id);
@@ -19,52 +31,79 @@ passport.deserializeUser(function(id, done) {
 	//});
 });
 
+function authorize(isRecognized, token, tokenSecret, done) {
+	if (isRecognized) {
+		return done(null, { id: "mike-twitter" });
+	} else {
+		return done("Not recognized");
+	}
+}
+
 passport.use(new TwitterStrategy({
-		consumerKey: TwitterCreds.key,
-		consumerSecret: TwitterCreds.secret,
+		consumerKey: CREDS.oauth.twitter.key,
+		consumerSecret: CREDS.oauth.twitter.secret,
 		callbackURL: "http://localhost:4444/auth/twitter/callback"
 	},
 	function(token, tokenSecret, profile, done) {
 
 		var id = profile.username;
-		var isRecognized = TwitterCreds.ids.indexOf(id) > -1;
+		var isRecognized = CREDS.oauth.twitter.ids.indexOf(id) > -1;
 
-		if (isRecognized) {
-			return done(null, { id: "mike-twitter" });
-		} else {
-			return done("Not recognized");
-		}
+		authorize(isRecognized, token, tokenSecret, done);
+
 	}
 ));
 passport.use(new GoogleStrategy({
-		clientID: GoogleCreds.key,
-		clientSecret: GoogleCreds.secret,
+		clientID: CREDS.oauth.google.key,
+		clientSecret: CREDS.oauth.google.secret,
 		callbackURL: "http://localhost:4444/auth/google/callback"
 	},
 	function(accessToken, refreshToken, profile, done) {
 
 		var emailAddresses = _.pluck(profile.emails, "value");
-		var isRecognized = _.intersection(GoogleCreds.ids, emailAddresses).length > 0;
+		var isRecognized = _.intersection(CREDS.oauth.google.ids, emailAddresses).length > 0;
 
-		if (isRecognized) {
-			return done(null, { id: "mike-google" });
-		} else {
-			return done("Not recognized");
-		}
+		authorize(isRecognized, accessToken, refreshToken, done);
 	}
 ));
 
-var app = express.createServer();
+var app = express();
+// Dynamically generating a secret in this way means one less file will have to
+// be managed outside of the repository. The drawback is that, in the event of
+// a server re-start, all authenticated users will be kicked and need to re-
+// authenticate.
+var sessionSecret = "This is a secret." + Math.random();;
+var sioCookieParser = express.cookieParser(sessionSecret);
+var store = new RedisStore();
 app.configure(function() {
-	app.use(express.cookieParser());
 	app.use(express.bodyParser());
-	app.use(express.session({ secret: "This is a secret" }));
+	app.use(express.cookieParser());
+	app.use(express.session({ store: store, secret: sessionSecret }));
 	app.use(passport.initialize());
 	app.use(passport.session());
 });
 
+// simulate a socket connection handler. In this case, only the raw "cookie"
+// header will be available. Attempt to parse it according to the server's
+// session secret to enable it was not tampered with by the client.
+function simulateSocket(cookie) {
+
+	var fakeReq = { headers: { cookie: cookie } };
+
+	sioCookieParser(fakeReq, {}, function(err) {
+		var sessionId = fakeReq.signedCookies["connect.sid"];
+		store.get(sessionId, function(err, data) {
+			console.log("Session data, retrieved by 'socket'", data);
+		});
+	});
+}
+
 app.get("/", function(req, res) {
 	var htmlStrs = ["Hello, world!"];
+
+	req.session.customCounter = (req.session.customCounter || 0) + 1;
+
+	simulateSocket(req.headers.cookie);
 
 	if (req.user) {
 		htmlStrs.push(JSON.stringify(req.user));
@@ -103,4 +142,5 @@ app.get("/logout", function(req, res) {
 	res.redirect("/");
 });
 
-app.listen(4444);
+http.createServer(app).listen(4444);
+https.createServer(CREDS.ssl, app).listen(4445);
